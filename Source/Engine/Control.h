b92803f562f91286aab800e0b9be2c48ff7bfdfa
Jason Addison <28279470+Jason-Addison@users.noreply.github.com>
2021-04-07 01:41:26 -0400

    Added documentation

--- Source/Engine/Control.h
+++ Source/Engine/Control.h
@@ -1,8 +1,16 @@
 #pragma once
+/**
+ * @brief Input control element for mouse, keyboard and other forms of controller.
+*/
 class Control
 {
 public:
 	Control();
+	/**
+	 * @brief Create new control
+	 * @param controllerType type of controller, ei: mouse, keyboard
+	 * @param id code of control
+	*/
 	Control(char controllerType, int id);
 	~Control();
 	int code;
